60d1bf8227463627b0a75c9997490f8972e30bf2
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-07-09 22:46:12 +0100

    Fix: Correcting user input prompt

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,7 +1,7 @@
-current_age = int(input("Enter your current age:"))
+current_age = input("Enter your current age:")
 current_year = 2023
 future_year = 2050 - current_year
 
-future_age = current_age + future_year
+future_age = int(current_age) + future_year
 
 print("In 2050, you will be", future_age, "years old.")
