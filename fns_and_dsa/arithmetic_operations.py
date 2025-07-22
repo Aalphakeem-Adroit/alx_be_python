6a7d4a3a464ab524d96d334f46039e6bff09b1df
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-07-22 23:58:36 +0100

    Fix: Checks for divide by zero scenario (elif statement)

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,8 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error: Division by zero is not allowed."
-            else:
+            elif num2 > 0:
                 return num1 / num2
+                
         case _:
             return "Error: Invalid operation. Please choose from add, subtract, multiply, or divide."
