af0fe1c5102eecdb9d21275d1bdbb18173f6eecb
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-07-22 23:54:06 +0100

    Task: Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+    """This is a function that performs basic arithmetic operations."""
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return "Error: Division by zero is not allowed."
+            else:
+                return num1 / num2
+        case _:
+            return "Error: Invalid operation. Please choose from add, subtract, multiply, or divide."
